f223782140751b9d1ef8cf9e96f1e7a47cc6eea5
DirtyBottle <479763003@qq.com>
2022-06-06 08:51:29 +0800

    v0.1

--- src/main/java/com/bottle/controller/UserController.java
+++ src/main/java/com/bottle/controller/UserController.java
@@ -5,7 +5,6 @@ import com.bottle.domain.User;
 import com.bottle.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
