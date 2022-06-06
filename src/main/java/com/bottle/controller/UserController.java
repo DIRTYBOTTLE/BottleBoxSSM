f1a5cfc8aa666443b3e08a0ce4eaf4b4853352d6
DirtyBottle <479763003@qq.com>
2022-06-06 09:09:09 +0800

    v0.1

--- src/main/java/com/bottle/controller/UserController.java
+++ src/main/java/com/bottle/controller/UserController.java
@@ -16,8 +16,12 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
+    private final UserService userService;
+
     @Autowired
-    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @RequestMapping("/login.do")
     @ResponseBody
